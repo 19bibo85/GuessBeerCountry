35376ea63de22f0f9ee86b6cc3f792ff39129d65
= <alberto.tosibrandi@gmail.com>
2015-03-18 21:22:29 +0000

    Added MainList and Setting

---
+++ src/com/example/GuessBeerCountry/Model/Beer.java
@@ -0,0 +1,81 @@
+package com.example.GuessBeerCountry.Model;
+
+/**
+ * Created by elyvitvit on 18/03/2015.
+ */
+public class Beer {
+
+    private String imgId;
+    private String name;
+    private String type;
+    private int difficulty;
+    private String country;
+    private String area;
+    private String continent;
+
+    public Beer(String imgId, String name, String type, int difficulty, String country, String area, String continent) {
+        this.imgId = imgId;
+        this.name = name;
+        this.type = type;
+        this.difficulty = difficulty;
+        this.country = country;
+        this.area = area;
+        this.continent = continent;
+    }
+
+    public String GetImgId() {
+        return this.imgId;
+    }
+
+    public String GetName() {
+        return this.name;
+    }
+
+    public String GetType() {
+        return this.type;
+    }
+
+    public int GetDifficulty() {
+        return this.difficulty;
+    }
+
+    public String GetCountry() {
+        return this.country;
+    }
+
+    public String GetArea() {
+        return this.area;
+    }
+
+    public String GetContinent() {
+        return this.continent;
+    }
+
+    public void SetImgId(String imgId) {
+        this.imgId = imgId;
+    }
+
+    public void SetName(String name) {
+        this.name = name;
+    }
+
+    public void SetType(String type) {
+        this.type = type;
+    }
+
+    public void SetDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void SetCountry(String country) {
+        this.country = country;
+    }
+
+    public void SetArea(String area) {
+        this.area = area;
+    }
+
+    public void SetContinent(String continent) {
+        this.continent = continent;
+    }
+}
